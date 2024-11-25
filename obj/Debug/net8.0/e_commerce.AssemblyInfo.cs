555953a6cb1048207613d9b3794d4e2b1d3912fb
Ege Altar Deniz <egealtar@river.com.tr>
2024-11-25 17:14:53 +0300

    stok uyarıusı ve speet birleştirme

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae43763d59932ecd4aeb3e1998814cbf704844c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e96e12516fe59a2af902f49ed7e9295b8f50fab")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
