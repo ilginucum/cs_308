343c175639ea77d0857578ac1646ce09934f47e4
cerenverim <cerenverimm@gmail.com>
2024-11-26 19:33:41 +0300

    proceed to checkout button

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5ec229eaaf3c02bc18ea161eefd8992328184c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87a3c276bfd94fe742909636eed8eaadd59347e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
