7c66a77a12776f5e2bdf230cf9957955c42e70bc
ilginucum <ilginucum@sabanciuniv.edu>
2024-11-26 14:11:24 +0300

    home front

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+336bdebfb45acf268cd71d7e07cb3a45fdcd5869")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7523a8ce011e90ea92d496108e0a30771c8fef4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
