b9441f401d59a7b4e6c8f16bf6a91bf256668f29
cerenverim <cerenverimm@gmail.com>
2024-11-26 17:34:01 +0300

    shopping cart footer

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c66a77a12776f5e2bdf230cf9957955c42e70bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50afaa821e2383f2affb506bb468f06865cc2f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
