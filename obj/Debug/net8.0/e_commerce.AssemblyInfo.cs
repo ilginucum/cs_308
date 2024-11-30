022f82aa2f74d3859deb522dd6f61d3f4cebf746
cerenverim <cerenverimm@gmail.com>
2024-11-30 19:27:33 +0300

    productdetails round-box +cmmnt, review...

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7a8b5b73bb815ae087e784ddb3c01dfbd492b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cac9ae9ba98477d3cc0df4f631eef6c289a68fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
