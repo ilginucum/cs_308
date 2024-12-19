57cb045c89d2aa103397af0f6dea706315badb96
cerenverim <cerenverimm@gmail.com>
2024-12-19 19:24:42 +0300

    wishist button back

--- obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e_commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e79658ac8680b0942c6c4c4a3a8ec37df2ae737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a7f6b694ea38fca5ec967c4c68b578121648cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e_commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
